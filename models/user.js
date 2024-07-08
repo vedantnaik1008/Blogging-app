eb51016539fcaedb45feeef0ec1008eac718c63f
tallman <vedunaik@gmail.com>
2024-07-08 11:45:30 +0530

    created signin

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-import mongoose, { model, Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 import { createHmac, randomBytes } from 'node:crypto';
 
 const userSchema = new Schema(
@@ -45,6 +45,22 @@ userSchema.pre("save", function (next) {
     this.password = hashedPassword
 
     next()
+})
+
+userSchema.static('matchPassword', async function(email, password){
+    const user = await this.findOne({email})
+    if(!user) throw new Error("User not found")
+
+    const salt = user.salt
+    const hashedPassword = user.password
+
+    const userProvidedHash = createHmac('sha256', salt)
+        .update(password)
+        .digest('hex');
+
+        if(hashedPassword !== userProvidedHash) throw new Error("Incorrect Password")
+
+        return user
 })
 
 export const User = model('user', userSchema)
